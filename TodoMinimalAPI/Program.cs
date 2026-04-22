750e54e8bbc4c2da8f50c6e0a45e5be8450a0452
automationhacks <automationhacks@gmail.com>
2026-04-22 07:08:41 +0530

    added API method to delete a todo

--- TodoMinimalAPI/Program.cs
+++ TodoMinimalAPI/Program.cs
@@ -5,13 +5,17 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
+// Get all todos
 app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
 
+// Get all completed todos
 app.MapGet("/todoitems/complete", async (TodoDb db) => await db.Todos.Where(todo => todo.IsComplete).ToListAsync());
 
+// Get todo by id
 app.MapGet("/todoitems/{id}",
     async (int id, TodoDb db) => await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
 
+// Create todo
 app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
 {
     db.Todos.Add(todo);
@@ -20,6 +24,7 @@ app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
+// Update todo
 app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
@@ -34,6 +39,19 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 
     await db.SaveChangesAsync();
     return Results.NoContent();
+});
+
+// Remove a todo from in memory db
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
 });
 
 app.Run();
