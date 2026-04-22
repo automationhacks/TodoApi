8b7d4c26ed87a8e824f05a62381c60791d985c9b
automationhacks <automationhacks@gmail.com>
2026-04-22 07:30:14 +0530

    Added swagger integration

--- TodoMinimalAPI/Program.cs
+++ TodoMinimalAPI/Program.cs
@@ -5,7 +5,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 // Enables displaying database related exceptions
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+// Adds swagger tooling
+// APIExplorer provides metadata about an HTTP API, it is used by Swagger to generate Swagger documents
+builder.Services.AddEndpointsApiExplorer();
+// Adds Swagger OpenAPI document generator to application services
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    /**
+    Enables swagger middleware for serving generated JSON doc and Swagger UI
+    It is only enabled in development environment
+    Exposing in production could expose sensitive details about API structure and implementation
+    **/
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
 // Get all todos
 app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
