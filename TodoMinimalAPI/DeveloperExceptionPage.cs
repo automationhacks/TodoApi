80a9332d6efc86a13a9f046b1bea9a29df03b41e
automationhacks <automationhacks@gmail.com>
2026-04-26 06:50:40 +0530

    Added example of a developer exception page

---
+++ TodoMinimalAPI/DeveloperExceptionPage.cs
@@ -0,0 +1,10 @@
+// Comment code in Program.cs and then run this app
+// Go to /exception to see Developer exception page
+// Otherwise keep this commented as there can only be one top level unit in the application
+
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+
+// app.MapGet("/exception", () => { throw new InvalidOperationException("Sample exception"); });
+// app.MapGet("/", () => "Test by calling /exception");
+// app.Run();
