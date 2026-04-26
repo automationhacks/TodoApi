38bf3e2b229c5cee364124337aa90495aa0c42b5
automationhacks <automationhacks@gmail.com>
2026-04-26 07:32:25 +0530

    Commented the exception handling logic demo

--- TodoMinimalAPI/DeveloperExceptionPage.cs
+++ TodoMinimalAPI/DeveloperExceptionPage.cs
@@ -3,41 +3,41 @@
 // Go to /exception to see Developer exception page
 // Otherwise keep this commented as there can only be one top level unit in the application
 
-var builder = WebApplication.CreateBuilder(args);
-// Generates problem details for all HTTP client and server error responses
-// that don't have body content yet
-builder.Services.AddProblemDetails();
+// var builder = WebApplication.CreateBuilder(args);
+// // Generates problem details for all HTTP client and server error responses
+// // that don't have body content yet
+// builder.Services.AddProblemDetails();
 
-var app = builder.Build();
+// var app = builder.Build();
 
-// In production env, we should use UseExceptionHandler that provides
-// RFC 7807 compliant payload to the client
-app.UseExceptionHandler(exceptionHandlerApp =>
-    exceptionHandlerApp.Run(async context => await Results.Problem().ExecuteAsync(context)));
-// Generates a problem details response by default
-// You can specify a fallback in case IProblemDetailsService implementation is not able to
-// generate a response
-// to test this:
-//      dotnet run DeveloperExceptionPage.cs
-//      curl -i -H "Accept: application/xml" http://localhost:5152/users/-1
+// // In production env, we should use UseExceptionHandler that provides
+// // RFC 7807 compliant payload to the client
+// app.UseExceptionHandler(exceptionHandlerApp =>
+//     exceptionHandlerApp.Run(async context => await Results.Problem().ExecuteAsync(context)));
+// // Generates a problem details response by default
+// // You can specify a fallback in case IProblemDetailsService implementation is not able to
+// // generate a response
+// // to test this:
+// //      dotnet run DeveloperExceptionPage.cs
+// //      curl -i -H "Accept: application/xml" http://localhost:5152/users/-1
 
-app.UseStatusCodePages(statusCodeHandlerApp =>
-{
-    statusCodeHandlerApp.Run(async httpContext =>
-    {
-        var pds = httpContext.RequestServices.GetService<IProblemDetailsService>();
-        if (pds == null || !await pds.TryWriteAsync(new() { HttpContext = httpContext }))
-        {
-            // Fallback behavior
-            await httpContext.Response.WriteAsync("Fallback: An error occured");
-        }
-    });
-});
+// app.UseStatusCodePages(statusCodeHandlerApp =>
+// {
+//     statusCodeHandlerApp.Run(async httpContext =>
+//     {
+//         var pds = httpContext.RequestServices.GetService<IProblemDetailsService>();
+//         if (pds == null || !await pds.TryWriteAsync(new() { HttpContext = httpContext }))
+//         {
+//             // Fallback behavior
+//             await httpContext.Response.WriteAsync("Fallback: An error occured");
+//         }
+//     });
+// });
 
-app.MapGet("/exception", () => { throw new InvalidOperationException("Sample exception"); });
-app.MapGet("/", () => "Test by calling /exception");
-app.MapGet("/users/{id:int}", (int id) => id <= 0 ? Results.BadRequest() : Results.Ok(new User(id)));
+// app.MapGet("/exception", () => { throw new InvalidOperationException("Sample exception"); });
+// app.MapGet("/", () => "Test by calling /exception");
+// app.MapGet("/users/{id:int}", (int id) => id <= 0 ? Results.BadRequest() : Results.Ok(new User(id)));
 
-app.Run();
+// app.Run();
 
-public record User(int Id);
+// public record User(int Id);
